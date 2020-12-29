e053c72739cbd5f003e3fa2c6f8c821ce8c194a8
sreesubbash <vostrosubbash97@hotmail.com>
2020-12-29 12:40:52 +0800

    Modify my_vrp.cc with tutorial code

--- examples/cpp/my_vrp.cc
+++ examples/cpp/my_vrp.cc
@@ -1,198 +1,220 @@
-// Copyright 2010-2018 Google LLC
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// [START program]
-// [START import]
-#include <string>
+#include <cmath>
 #include <vector>
 
 #include "ortools/constraint_solver/routing.h"
 #include "ortools/constraint_solver/routing_enums.pb.h"
 #include "ortools/constraint_solver/routing_index_manager.h"
 #include "ortools/constraint_solver/routing_parameters.h"
-// [END import]
 
-// [START program_part1]
 namespace operations_research {
-// [START data_model]
-struct DataModel {
-  const std::vector<std::vector<int64>> time_matrix{
-      {0, 6, 9, 8, 7, 3, 6, 2, 3, 2, 6, 6, 4, 4, 5, 9, 7},
-      {6, 0, 8, 3, 2, 6, 8, 4, 8, 8, 13, 7, 5, 8, 12, 10, 14},
-      {9, 8, 0, 11, 10, 6, 3, 9, 5, 8, 4, 15, 14, 13, 9, 18, 9},
-      {8, 3, 11, 0, 1, 7, 10, 6, 10, 10, 14, 6, 7, 9, 14, 6, 16},
-      {7, 2, 10, 1, 0, 6, 9, 4, 8, 9, 13, 4, 6, 8, 12, 8, 14},
-      {3, 6, 6, 7, 6, 0, 2, 3, 2, 2, 7, 9, 7, 7, 6, 12, 8},
-      {6, 8, 3, 10, 9, 2, 0, 6, 2, 5, 4, 12, 10, 10, 6, 15, 5},
-      {2, 4, 9, 6, 4, 3, 6, 0, 4, 4, 8, 5, 4, 3, 7, 8, 10},
-      {3, 8, 5, 10, 8, 2, 2, 4, 0, 3, 4, 9, 8, 7, 3, 13, 6},
-      {2, 8, 8, 10, 9, 2, 5, 4, 3, 0, 4, 6, 5, 4, 3, 9, 5},
-      {6, 13, 4, 14, 13, 7, 4, 8, 4, 4, 0, 10, 9, 8, 4, 13, 4},
-      {6, 7, 15, 6, 4, 9, 12, 5, 9, 6, 10, 0, 1, 3, 7, 3, 10},
-      {4, 5, 14, 7, 6, 7, 10, 4, 8, 5, 9, 1, 0, 2, 6, 4, 8},
-      {4, 8, 13, 9, 8, 7, 10, 3, 7, 4, 8, 3, 2, 0, 4, 5, 6},
-      {5, 12, 9, 14, 12, 6, 6, 7, 3, 3, 4, 7, 6, 4, 0, 9, 2},
-      {9, 10, 18, 6, 8, 12, 15, 8, 13, 9, 13, 3, 4, 5, 9, 0, 9},
-      {7, 14, 9, 16, 14, 8, 5, 10, 6, 5, 4, 10, 8, 6, 2, 9, 0},
-  };
-  const std::vector<std::pair<int64, int64>> time_windows{
-      {0, 5},    // depot
-      {7, 12},   // 1
-      {10, 15},  // 2
-      {16, 18},  // 3
-      {10, 13},  // 4
-      {0, 5},    // 5
-      {5, 10},   // 6
-      {0, 4},    // 7
-      {5, 10},   // 8
-      {0, 3},    // 9
-      {10, 16},  // 10
-      {10, 15},  // 11
-      {0, 5},    // 12
-      {5, 10},   // 13
-      {7, 8},    // 14
-      {10, 15},  // 15
-      {11, 15},  // 16
-  };
-  const int num_vehicles = 5;
-  const RoutingIndexManager::NodeIndex depot{0};
-};
-// [END data_model]
 
-// [START solution_printer]
+
+
+
+struct DataModel {
+  const std::vector<std::vector<int64>> distance_matrix{
+      {0, 548, 776, 696, 582, 274, 502, 194, 308, 194, 536, 502, 388, 354, 468,
+       776, 662},
+      {548, 0, 684, 308, 194, 502, 730, 354, 696, 742, 1084, 594, 480, 674,
+       1016, 868, 1210},
+      {776, 684, 0, 992, 878, 502, 274, 810, 468, 742, 400, 1278, 1164, 1130,
+       788, 1552, 754},
+      {696, 308, 992, 0, 114, 650, 878, 502, 844, 890, 1232, 514, 628, 822,
+       1164, 560, 1358},
+      {582, 194, 878, 114, 0, 536, 764, 388, 730, 776, 1118, 400, 514, 708,
+       1050, 674, 1244},
+      {274, 502, 502, 650, 536, 0, 228, 308, 194, 240, 582, 776, 662, 628, 514,
+       1050, 708},
+      {502, 730, 274, 878, 764, 228, 0, 536, 194, 468, 354, 1004, 890, 856, 514,
+       1278, 480},
+      {194, 354, 810, 502, 388, 308, 536, 0, 342, 388, 730, 468, 354, 320, 662,
+       742, 856},
+      {308, 696, 468, 844, 730, 194, 194, 342, 0, 274, 388, 810, 696, 662, 320,
+       1084, 514},
+      {194, 742, 742, 890, 776, 240, 468, 388, 274, 0, 342, 536, 422, 388, 274,
+       810, 468},
+      {536, 1084, 400, 1232, 1118, 582, 354, 730, 388, 342, 0, 878, 764, 730,
+       388, 1152, 354},
+      {502, 594, 1278, 514, 400, 776, 1004, 468, 810, 536, 878, 0, 114, 308,
+       650, 274, 844},
+      {388, 480, 1164, 628, 514, 662, 890, 354, 696, 422, 764, 114, 0, 194, 536,
+       388, 730},
+      {354, 674, 1130, 822, 708, 628, 856, 320, 662, 388, 730, 308, 194, 0, 342,
+       422, 536},
+      {468, 1016, 788, 1164, 1050, 514, 514, 662, 320, 274, 388, 650, 536, 342,
+       0, 764, 194},
+      {776, 868, 1552, 560, 674, 1050, 1278, 742, 1084, 810, 1152, 274, 388,
+       422, 764, 0, 798},
+      {662, 1210, 754, 1358, 1244, 708, 480, 856, 514, 468, 354, 844, 730, 536,
+       194, 798, 0},
+  };
+    const std::vector<std::vector<RoutingIndexManager::NodeIndex>>
+      pickups_deliveries{
+          {RoutingIndexManager::NodeIndex{1},
+           RoutingIndexManager::NodeIndex{6}},
+          {RoutingIndexManager::NodeIndex{2},
+           RoutingIndexManager::NodeIndex{10}},
+          {RoutingIndexManager::NodeIndex{4},
+           RoutingIndexManager::NodeIndex{3}},
+          {RoutingIndexManager::NodeIndex{5},
+           RoutingIndexManager::NodeIndex{9}},
+          {RoutingIndexManager::NodeIndex{7},
+           RoutingIndexManager::NodeIndex{8}},
+          {RoutingIndexManager::NodeIndex{15},
+           RoutingIndexManager::NodeIndex{11}},
+          {RoutingIndexManager::NodeIndex{13},
+           RoutingIndexManager::NodeIndex{12}},
+          {RoutingIndexManager::NodeIndex{16},
+           RoutingIndexManager::NodeIndex{14}},
+      };
+  const int num_vehicles = 4;
+  const RoutingIndexManager::NodeIndex depot{0};
+  const std::vector<int64> demands{
+    0, 1, 1, 2, 4, 2, 4, 8, 8, 1, 2, 1, 2, 4, 4, 8, 8,
+  };
+  const std::vector<int64> vehicle_capacities{30, 30, 30, 30};
+};
+
+
+
 //! @brief Print the solution.
 //! @param[in] data Data of the problem.
 //! @param[in] manager Index manager used.
 //! @param[in] routing Routing solver used.
 //! @param[in] solution Solution found by the solver.
-void PrintSolution(const DataModel& data, const RoutingIndexManager& manager,
+void VRPPrintSolution(const DataModel& data, const RoutingIndexManager& manager,
                    const RoutingModel& routing, const Assignment& solution) {
-  const RoutingDimension& time_dimension = routing.GetDimensionOrDie("Time");
-  int64 total_time{0};
+  int64 max_route_distance{0};
   for (int vehicle_id = 0; vehicle_id < data.num_vehicles; ++vehicle_id) {
     int64 index = routing.Start(vehicle_id);
-    LOG(INFO) << "Route for vehicle " << vehicle_id << ":";
-    std::ostringstream route;
+    LOG(INFO) << "Route for Vehicle " << vehicle_id << ":";
+    int64 route_distance{0};
+    std::stringstream route;
     while (routing.IsEnd(index) == false) {
-      auto time_var = time_dimension.CumulVar(index);
-      route << manager.IndexToNode(index).value() << " Time("
-            << solution.Min(time_var) << ", " << solution.Max(time_var)
-            << ") -> ";
+      route << manager.IndexToNode(index).value() << " -> ";
+      int64 previous_index = index;
       index = solution.Value(routing.NextVar(index));
+      route_distance += routing.GetArcCostForVehicle(previous_index, index,
+                                                     int64{vehicle_id});
     }
-    auto time_var = time_dimension.CumulVar(index);
-    LOG(INFO) << route.str() << manager.IndexToNode(index).value() << " Time("
-              << solution.Min(time_var) << ", " << solution.Max(time_var)
-              << ")";
-    LOG(INFO) << "Time of the route: " << solution.Min(time_var) << "min";
-    total_time += solution.Min(time_var);
+    LOG(INFO) << route.str() << manager.IndexToNode(index).value();
+    LOG(INFO) << "Distance of the route: " << route_distance << "m";
+    max_route_distance = std::max(route_distance, max_route_distance);
   }
-  LOG(INFO) << "Total time of all routes: " << total_time << "min";
+  LOG(INFO) << "Maximum of the route distances: " << max_route_distance << "m";
   LOG(INFO) << "";
-  LOG(INFO) << "Advanced usage:";
   LOG(INFO) << "Problem solved in " << routing.solver()->wall_time() << "ms";
 }
-// [END solution_printer]
 
-void VrpTimeWindows() {
+void VrpGlobalSpan() {
+  std::cout << "Subbash running: wait for a few seconds" << std::endl;
+  // Time window example from or tools documentation is not yet run here
+  // Also duration taken to load and unload is missing here
+
   // Instantiate the data problem.
-  // [START data]
   DataModel data;
-  // [END data]
 
   // Create Routing Index Manager
-  // [START index_manager]
-  RoutingIndexManager manager(data.time_matrix.size(), data.num_vehicles,
+  RoutingIndexManager manager(data.distance_matrix.size(), data.num_vehicles,
                               data.depot);
-  // [END index_manager]
 
   // Create Routing Model.
-  // [START routing_model]
   RoutingModel routing(manager);
-  // [END routing_model]
 
   // Create and register a transit callback.
-  // [START transit_callback]
   const int transit_callback_index = routing.RegisterTransitCallback(
       [&data, &manager](int64 from_index, int64 to_index) -> int64 {
-        // Convert from routing variable Index to time matrix NodeIndex.
+        // Convert from routing variable Index to distance matrix NodeIndex.
         auto from_node = manager.IndexToNode(from_index).value();
         auto to_node = manager.IndexToNode(to_index).value();
-        return data.time_matrix[from_node][to_node];
+        return data.distance_matrix[from_node][to_node];
       });
-  // [END transit_callback]
+  
+  const int demand_callback_index = routing.RegisterUnaryTransitCallback(
+    [&data, &manager](int64 from_index) -> int64 {
+      // Convert from routing variable Index to demand NodeIndex.
+      int from_node = manager.IndexToNode(from_index).value();
+      return data.demands[from_node];
+    });
 
   // Define cost of each arc.
-  // [START arc_cost]
   routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index);
-  // [END arc_cost]
 
-  // Add Time constraint.
-  // [START time_constraint]
-  std::string time{"Time"};
-  routing.AddDimension(transit_callback_index,  // transit callback index
-                       int64{30},               // allow waiting time
-                       int64{30},               // maximum time per vehicle
-                       false,  // Don't force start cumul to zero
-                       time);
-  const RoutingDimension& time_dimension = routing.GetDimensionOrDie(time);
-  // Add time window constraints for each location except depot.
-  for (int i = 1; i < data.time_windows.size(); ++i) {
-    int64 index = manager.NodeToIndex(RoutingIndexManager::NodeIndex(i));
-    time_dimension.CumulVar(index)->SetRange(data.time_windows[i].first,
-                                             data.time_windows[i].second);
-  }
-  // Add time window constraints for each vehicle start node.
-  for (int i = 0; i < data.num_vehicles; ++i) {
-    int64 index = routing.Start(i);
-    time_dimension.CumulVar(index)->SetRange(data.time_windows[0].first,
-                                             data.time_windows[0].second);
-  }
-  // [END time_constraint]
+  // Add Distance constraint.
+  routing.AddDimension(transit_callback_index, 0,
+                       3000,  // maximum distance per vehicle?
+                       true,  // start cumul to zero
+                       "Distance");
+  RoutingDimension* distance_dimension = routing.GetMutableDimension("Distance");
+  distance_dimension->SetGlobalSpanCostCoefficient(100);
 
-  // Instantiate route start and end times to produce feasible times.
-  // [START depot_start_end_times]
-  for (int i = 0; i < data.num_vehicles; ++i) {
-    routing.AddVariableMinimizedByFinalizer(
-        time_dimension.CumulVar(routing.Start(i)));
-    routing.AddVariableMinimizedByFinalizer(
-        time_dimension.CumulVar(routing.End(i)));
+  // More general compared to AddDimension
+  routing.AddDimensionWithVehicleCapacity(
+    demand_callback_index,    // transit callback index
+    int64{0},                 // null capacity slack
+    data.vehicle_capacities,  // vehicle maximum capacities
+    true,                     // start cumul to zero
+    "Capacity");
+
+  
+  Solver* const solver = routing.solver();
+
+  // Define Transportation Requests.
+  for (const auto& request : data.pickups_deliveries) {
+    int64 pickup_index = manager.NodeToIndex(request[0]);
+    int64 delivery_index = manager.NodeToIndex(request[1]);
+    routing.AddPickupAndDelivery(pickup_index, delivery_index);
+    solver->AddConstraint(solver->MakeEquality(
+        routing.VehicleVar(pickup_index), routing.VehicleVar(delivery_index)));
+    solver->AddConstraint(
+        solver->MakeLessOrEqual(distance_dimension->CumulVar(pickup_index),
+                                distance_dimension->CumulVar(delivery_index)));
   }
-  // [END depot_start_end_times]
 
   // Setting first solution heuristic.
-  // [START parameters]
   RoutingSearchParameters searchParameters = DefaultRoutingSearchParameters();
+  // searchParameters.set_first_solution_strategy(
+  //    FirstSolutionStrategy::PARALLEL_CHEAPEST_INSERTION);
   searchParameters.set_first_solution_strategy(
       FirstSolutionStrategy::PATH_CHEAPEST_ARC);
-  // [END parameters]
+  searchParameters.set_local_search_metaheuristic(
+      LocalSearchMetaheuristic::GUIDED_LOCAL_SEARCH);
+  searchParameters.mutable_time_limit()->set_seconds(4);
+  //searchParameters.set_log_search(true);
+
+
 
   // Solve the problem.
-  // [START solve]
   const Assignment* solution = routing.SolveWithParameters(searchParameters);
-  // [END solve]
 
   // Print solution on console.
-  // [START print_solution]
-  PrintSolution(data, manager, routing, *solution);
-  // [END print_solution]
+  if (routing.status() == 1) {
+    VRPPrintSolution(data, manager, routing, *solution);
+  } else {
+    std::cout << "solver status is :: " << routing.status() << std::endl;
+  }
+  
+  // Check if solution is not nullptr first else it will have segmentation fault
+  //std::cout << solution << std::endl;
+
+  
+  /*
+  std::vector<std::vector<int>> answer = GetRoutes(manager, routing, *solution);
+  for (std::vector<int> row : answer) {
+    for (int i : row) {
+      std::cout << i << " - ";
+    }
+    std::cout << std::endl;
+  }
+  */
 }
+
+
+
 }  // namespace operations_research
 
 int main(int argc, char** argv) {
-  operations_research::VrpTimeWindows();
-  std::cout << "This was my file 6" << std::endl;
+  //operations_research::Tsp2();
+  operations_research::VrpGlobalSpan();
   return EXIT_SUCCESS;
 }
-// [END program_part1]
-// [END program]
